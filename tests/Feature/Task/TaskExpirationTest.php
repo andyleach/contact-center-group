0c233bcd50c585757abc4f1dcc49d85ec204dd5f
Andrew Leach <andrew.leach@bettercarpeople.com>
2021-10-04 14:03:34 -0400

    Task Tests Initial

---
+++ tests/Feature/Task/TaskExpirationTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature\Task;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class TaskExpirationTest extends TestCase {
+    use RefreshDatabase;
+
+    /**
+     *
+     */
+    public function test_a_task_can_be_assigned_to_a_user() {
+        $user = User::factory()->create();
+
+        $this->assertTrue(true);
+    }
+
+    /**
+     *
+     */
+    public function test_a_task_assignment_can_be_cancelled() {
+        $this->assertTrue(true);
+    }
+
+    /**
+     *
+     */
+    public function test_a_task_can_expire() {
+        $this->assertTrue(true);
+    }
+}
