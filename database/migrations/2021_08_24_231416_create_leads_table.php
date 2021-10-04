06b4b4ad308c7468910a0991fcb05b7d53c2febf
Andrew Leach <andrew.leach@bettercarpeople.com>
2021-10-04 08:23:26 -0400

    Consolidate Lead Tables

--- database/migrations/2021_08_24_231416_create_leads_table.php
+++ database/migrations/2021_08_24_231416_create_leads_table.php
@@ -15,6 +15,21 @@ class CreateLeadsTable extends Migration
      */
     public function up()
     {
+        Schema::create('lead_dispositions', function (Blueprint $table) {
+            $table->id();
+            $table->string('label')->unique();
+            $table->string('description', 255)->default('');
+            $table->timestamps();
+        });
+
+        Schema::create('lead_statuses', function (Blueprint $table) {
+            $table->id();
+            $table->string('label')->unique();
+            $table->string('description', 255)->default('');
+            $table->boolean('is_billable')->default(false);
+            $table->timestamps();
+        });
+
         Schema::create('leads', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(LeadStatus::class, 'lead_status_id');
@@ -24,6 +39,8 @@ class CreateLeadsTable extends Migration
             $table->string('full_name');
             $table->timestamps();
         });
+
+        $this->initializeLeadStatuses();
     }
 
     /**
@@ -33,6 +50,61 @@ class CreateLeadsTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('lead_statuses');
+        Schema::dropIfExists('lead_dispositions');
         Schema::dropIfExists('leads');
+    }
+
+    public function initializeLeadStatuses() {
+        LeadStatus::query()->insert([
+            [
+                'id' => LeadStatus::PENDING,
+                'label' => 'Pending',
+                'description' => 'The lead has been created, but work has not begun.',
+                'is_billable' => false,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => LeadStatus::WORKING,
+                'label' => 'Working',
+                'description' => 'We have begun working on the lead in our system.',
+                'is_billable' => false,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => LeadStatus::COMPLETED,
+                'label' => 'Completed',
+                'description' => 'We have completed working the lead.',
+                'is_billable' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => LeadStatus::CLOSED_AGED,
+                'label' => 'Closed (Aged)',
+                'description' => 'We have closed the lead because it aged out.',
+                'is_billable' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => LeadStatus::CLOSED_SUBSCRIPTION_TERMINATED,
+                'label' => 'Closed (Subscription Terminated',
+                'description' => 'We have closed out the lead because we are no longer working with the client.',
+                'is_billable' => true,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id' => LeadStatus::DISMISSED,
+                'label' => 'Dismissed',
+                'description' => 'We have decided that we will not work the lead.',
+                'is_billable' => false,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
     }
 }
