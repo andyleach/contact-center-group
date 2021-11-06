ed2a8eed62c7a548d785b23a96e5716c0724698b
Andrew Leach <andrew.leach@bettercarpeople.com>
2021-11-06 11:52:55 -0400

    Update MatchTaskToAnExistingLead.php

--- app/Jobs/MatchTaskToAnExistingLead.php
+++ app/Jobs/MatchTaskToAnExistingLead.php
@@ -15,12 +15,16 @@ use Illuminate\Support\Collection;
 
 /**
  * Takes a new task that is unassociated with a lead in our system and make a good faith attempt at finding an existing
- * lead in our system and assign that task to the lead.
+ * lead in our system and assign that task to the lead.  The lead matching we perform here DOES NOT guarantee that it
+ * will be the lead that ultimately gets assigned to this task, we are just attempting to help the agent by
+ * pre-assigning the lead most likely to be tied to this.
  *
  * Behavior:
- * - If we were unable to find a lead that matched it we will create a new lead
  * - If we found a lead in the system that matched it, assign the task to that lead
  *   - If that lead does not have a customer, require agent to match that lead to a customer before wrapping up
+ *   - Make the task ready for work
+ * - If we didn't find a lead to match it to,
+ *   - Make the task ready for work
  */
 class MatchTaskToAnExistingLead implements ShouldQueue
 {
