a9f2c1ff3f8aedd1f81b7743a4923a5f97baf52f
Andrew Leach <andrew.leach@bettercarpeople.com>
2021-11-20 09:30:15 -0500

    Update Edit.vue

--- resources/js/Pages/Client/Edit.vue
+++ resources/js/Pages/Client/Edit.vue
@@ -18,7 +18,7 @@
 
         <jet-action-section>
             <template #title>
-                Client Configuration
+                Phone Number Configuration
             </template>
 
             <template #description>
@@ -29,7 +29,7 @@
                 <div class="-ml-4 -mt-2 flex mb-4 items-center justify-between flex-wrap sm:flex-nowrap">
                     <div class="ml-4 mt-2">
                         <h3 class="text-lg leading-6 font-medium text-gray-900">
-                            Client Phone Numbers
+                            Phone Numbers
                         </h3>
                     </div>
                     <div class="ml-4 mt-2 flex-shrink-0">
